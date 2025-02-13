6b18b862a8059d001a95ab16ea87e8046f414037
CHARANRAJ PATTAR <68002663+charanrajsp@users.noreply.github.com>
2025-02-14 00:27:16 +0530

    Update SignIn.jsx

--- src/page/SignIn.jsx
+++ src/page/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Signin.css";
+import "./signin.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
